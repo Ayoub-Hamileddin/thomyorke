17b928ae87255482ed50181ab6c19a37414de7b8
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-07-19 12:00:00 +0100

    Commit for 19 July

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import jsonfile from "jsonfile";
 import moment from "moment";
 
 const path = "./data.json";
-const date = moment("2025-07-19").format();
+const date = moment().format();
 
 const data = {
   date: date,
